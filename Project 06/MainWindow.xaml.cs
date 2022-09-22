da8dc7186a1133f5eafdec42b242fa31be30bdfe
Ssorreadd <ssorreadd@gmail.com>
2022-09-22 06:29:35 +0300

    Pre-Final Commit

--- Project 06/MainWindow.xaml.cs
+++ Project 06/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Project_06
 {
@@ -24,53 +12,90 @@ namespace Project_06
         {
             InitializeComponent();
 
-            Init();
+            VertBar.Text = "\n\n2.0 \n\n\n1.5 \n\n\n1.0 \n\n\n0.5 ";
 
+            Start();
         }
 
-        private void Init()
+        private void Repeat_Click(object sender, RoutedEventArgs e) => Start();
+
+        private void Start()
         {
-            double[] x = new double[60];
+            //Оригинальная последовательность.
+            double[] originalSequence = new double[60];
 
             Random random = new Random();
 
-            //заполнение
-            for (int i = 0; i < x.Length; i++)
+            //Заполнение.
+            for (int i = 0; i < originalSequence.Length; i++)
             {
-                x[i] = -1/0.8 * Math.Log10(random.Next());
+                //60 случайных чисел с экспоненци-альным законом распределения с параметром 0,8.
+                originalSequence[i] = -1/0.8 * Math.Log(random.Next());
             }
 
             //сортировка по возрастанию
-            for (int i = 0; i < x.Length; i++)
-            {
-                //Вложенный цикл (количество повторений, равно количеству элементов массива минус 1 и минус количество выполненных повторений основного цикла)
-                for (int j = 0; j < x.Length - 1 - i; j++)
+            for (int i = 0; i < originalSequence.Length; i++)
+            { 
+                for (int j = 0; j < originalSequence.Length - 1 - i; j++)
                 {
-                    //Если элемент массива с индексом j больше следующего за ним элемента
-                    if (x[j] > x[j + 1])
+                    if (originalSequence[j] > originalSequence[j + 1])
                     {
-                        //Меняем местами элемент массива с индексом j и следующий за ним
-                        Swap(ref x[j], ref x[j + 1]);
+                        Swap(ref originalSequence[j], ref originalSequence[j + 1]);
                     }
                 }
             }
 
+            //Новая последовательность
+            double[] newSequence = new double[30];
 
-            double[] newCollection = new double[30];
-
-            for (int i = x.Length / 2 - 1; i >= 0; i--)
+            int newSequenceCount = 29;
+            //Образовать новую последовательность, состоящую из разности соседних элементов Xi - Xi-1
+            for (int i = originalSequence.Length - 1; i > 0; i--)
             {
-                if (i != 0)
-                {
-                    newCollection[i] = x[i] - x[i - 1];
-                }
-                else
-                {
-                    x[i] = x[i] - x[i + 1];
-                }
-              
+                newSequence[newSequenceCount] = Math.Round(originalSequence[i] - originalSequence[i - 1], 3);
+
+                i--;
+                newSequenceCount--;
             }
 
+            //среднее значение
+            double sampleMean = 0;
+            for (int i = 0; i < newSequence.Length; i++)
+            {
+                sampleMean += newSequence[i];
+            }
+
+            sampleMean = Math.Round(sampleMean / newSequence.Length, 3);
+
+            //Вычитание выборочного среднего из каждого значения в выборке и возведение в квадрат.
+            double[] samples = new double[newSequence.Length];
+            for (int i = 0; i < samples.Length; i++)
+            {
+                samples[i] = Math.Round(Math.Pow(newSequence[i] - sampleMean, 2), 2);
+            }
+
+            //Вычисление суммы квадратов разностей ряда.
+            double sum = 0;
+            for (int i = 0; i < samples.Length; i++)
+            {
+                sum += samples[i];
+            }
+
+            //Дисперсия.
+            double dispersion = sum / samples.Length - 1;
+
+            //Заполнение и создание гистограммы.
+            SampleMeanLabel.Content = $"Ср. значение: {Math.Round(sampleMean, 3)}";
+            DispersionLabel.Content = $"Дисперсия: {Math.Round(dispersion, 3)}";
+
+            Bar1.Height = newSequence[0] * 100;
+            Bar1Value.Text = newSequence[0].ToString();
+
+            Bar2.Height = newSequence[9] * 100;
+            Bar2Value.Text = newSequence[9].ToString();
+
+            Bar3.Height = newSequence[29] * 100;
+            Bar3Value.Text = newSequence[29].ToString();
         }
 
         public static void Swap(ref double aFirstArg, ref double aSecondArg)
@@ -81,6 +106,5 @@ namespace Project_06
 
             aSecondArg = tmpParam;
         }
-
     }
 }
